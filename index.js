1103a3433c00f875679b8cd8e2be0888948cff42
chokihub <qdmy.75@gmail.com>
2017-01-23 03:39:59 +0900

    Error Sound

--- index.js
+++ index.js
@@ -32,19 +32,19 @@ class NICTalk {
   speak(...argvs) {
     argvs = typeof argvs[0] === 'object' ? argvs[0] : argvs;
     var file = argvs[0],
-    text = argvs.length > 1 ? argvs[1] : '',
-    callback = argvs.length > 1 ? argvs[argvs.length - 1] : '',
-    path = this.directory + file + '.wav';
+        text = argvs.length > 1 ? argvs[1] : '',
+        callback = argvs.length > 1 ? argvs[argvs.length - 1] : '',
+        path = this.directory + file + '.wav';
     argvs.length === 2 && (typeof argvs[1] === 'function' ? text = '' : callback = '');
-
+    
     var postData = JSON.stringify({
-      method: 'speak',
-      params: [this.version, {
-        'language': this.language,
-        'text': text,
-        'voiceType': this.voiceType,
-        'audioType': this.audioType
-      }]
+          method: 'speak',
+          params: [this.version, {
+            'language': this.language,
+            'text': text,
+            'voiceType': this.voiceType,
+            'audioType': this.audioType
+          }]
     });
     text ? _getSound(path, postData, callback) : _playSound(path, callback);
   }
@@ -52,11 +52,11 @@ class NICTalk {
 
 var _playSound = (...argv) => {
   var mediaSoundPlayer = '"(New-Object System.Media.SoundPlayer "' + filepath + '").PlaySync()',
-  player = process.platform === 'darwin' ? 'afplay' : 'aplay',
-  filepath = typeof argv[0] === 'string' ? [argv[0]] : argv[0],
-  callback = argv[1] ? argv[1] : (error) => {
-    if (error) throw error;
-  };
+      player = process.platform === 'darwin' ? 'afplay' : 'aplay',
+      filepath = typeof argv[0] === 'string' ? [argv[0]] : argv[0],
+      callback = argv[1] ? argv[1] : (error) => {
+        if (error) throw error;
+      };
 
   return process.platform === 'win32' ?
   childProcess.exec('powershell.exe ' + mediaSoundPlayer, callback) :
@@ -65,29 +65,24 @@ var _playSound = (...argv) => {
 
 var _getSound = (path, postData, callback) => {
   var options = {
-    hostname: 'rospeex.ucri.jgn-x.jp',
-    method: 'POST',
-    path: '/nauth_json/jsServices/VoiceTraSS',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(postData)
-    }
+      hostname: 'rospeex.ucri.jgn-x.jp',
+      method: 'POST',
+      path: '/nauth_json/jsServices/VoiceTraSS',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(postData)
+      }
   };
 
   var req = http.request(options, (res) => {
     var data = "";
     res.setEncoding('utf8')
     .on('data', (chunk) => data += chunk)
-    .on('end', () => {
-      try {
-        fs.writeFile(path, JSON.parse(data).result.audio, 'base64', () => {
-          return _playSound(path, callback);
-        });
-      } catch (e) {
-        return console.log(e);
-      }
-    });
-  }).on('error', (e) => console.log(e));
+    .on('end', () =>
+      fs.writeFile(path, JSON.parse(data).result.audio, 'base64', (e) =>
+        e ? _playSound(__dirname+"/error.wav", console.log(e)) : _playSound(path, callback))
+    );
+  }).on('error', (e) => _playSound(__dirname+"/error.wav", console.log(e)));
   req.write(postData);
   req.end();
 };
